c1c8d8e16d6b766603568eef03ae416f415dbd46
harryxu <harryzhxu@gmail.com>
2024-03-04 07:40:31 +0000

    Add pyenv path

--- fish/config.fish
+++ fish/config.fish
@@ -6,4 +6,15 @@ if type -q nvim
   alias vi='nvim'
 end
 
+if test -e ~/.local/bin
+  fish_add_path -m ~/.local/bin
+end
+
+# pyenv
+set -Ux PYENV_ROOT $HOME/.pyenv
+if test -e $PYENV_ROOT
+  fish_add_path -m $PYENV_ROOT/bin
+  pyenv init - | source
+end
+
 starship init fish | source
