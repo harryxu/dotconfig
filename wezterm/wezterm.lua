4b6174c6572eaf5481f5cb8d2a02fd224ecd1390
harryxu <harryzhxu@gmail.com>
2024-12-27 15:23:15 +0800

    wezterm: Adjust tab config.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -47,56 +47,6 @@ local function brightness_auto_adjust(hex, amount)
   return new_hex
 end
 
-local function getDirectoryName(path)
-  if not path then
-    return 'Unknown'
-  end
-  -- Remove any trailing slashes from the path
-  path = path:gsub("/+$", "")
-  -- Extract the last part of the path (the directory name)
-  local directoryName = path:match("([^/]+)$")
-  return directoryName or 'Unknown'
-end
-
-local process_icons = { -- for get_process function
-  ['docker'] = wezterm.nerdfonts.linux_docker,
-  ['docker-compose'] = wezterm.nerdfonts.linux_docker,
-  ['psql'] = wezterm.nerdfonts.dev_postgresql,
-  ['kuberlr'] = wezterm.nerdfonts.linux_docker,
-  ['kubectl'] = wezterm.nerdfonts.linux_docker,
-  ['stern'] = wezterm.nerdfonts.linux_docker,
-  ['nvim'] = wezterm.nerdfonts.custom_vim,
-  ['make'] = wezterm.nerdfonts.seti_makefile,
-  ['vim'] = wezterm.nerdfonts.dev_vim,
-  ['go'] = wezterm.nerdfonts.seti_go,
-  ['zsh'] = wezterm.nerdfonts.dev_terminal,
-  ['bash'] = wezterm.nerdfonts.cod_terminal_bash,
-  ['btm'] = wezterm.nerdfonts.mdi_chart_donut_variant,
-  ['htop'] = wezterm.nerdfonts.mdi_chart_donut_variant,
-  ['cargo'] = wezterm.nerdfonts.dev_rust,
-  ['sudo'] = wezterm.nerdfonts.fa_hashtag,
-  ['lazydocker'] = wezterm.nerdfonts.linux_docker,
-  ['git'] = wezterm.nerdfonts.dev_git,
-  ['lua'] = wezterm.nerdfonts.seti_lua,
-  ['wget'] = wezterm.nerdfonts.mdi_arrow_down_box,
-  ['curl'] = wezterm.nerdfonts.mdi_flattr,
-  ['gh'] = wezterm.nerdfonts.dev_github_badge,
-  ['ruby'] = wezterm.nerdfonts.cod_ruby,
-  ['pwsh'] = wezterm.nerdfonts.seti_powershell,
-  ['node'] = wezterm.nerdfonts.dev_nodejs_small,
-  ['dotnet'] = wezterm.nerdfonts.md_language_csharp,
-}
-
-local function get_process(tab)
-  local process_name = tab.active_pane.foreground_process_name:match("([^/\\]+)%.exe$") or
-      tab.active_pane.foreground_process_name:match("([^/\\]+)$")
-
-  -- local icon = process_icons[process_name] or string.format('[%s]', process_name)
-  local icon = process_icons[process_name] or ''
-
-  return icon
-end
-
 -- This table will hold the configuration.
 local config = {}
 
@@ -113,7 +63,7 @@ local scheme_def = wezterm.color.get_builtin_schemes()[scheme]
 config.color_scheme = scheme
 
 config.font = GetFont("IosevkaTerm Nerd Font", 500)
-config.font_size = 23.0
+config.font_size = 25.0
 
 config.command_palette_font_size = 19
 
@@ -146,26 +96,13 @@ config.tab_bar_at_bottom = false
 config.hide_tab_bar_if_only_one_tab = false
 
 config.window_frame = {
-    -- The font used in the tab bar.
     -- Roboto Bold is the default; this font is bundled
-    -- with wezterm.
-    -- Whatever font is selected here, it will have the
-    -- main font setting appended to it to pick up any
-    -- fallback fonts you may have used there.
     font = wezterm.font { family = 'Roboto', weight = 500 },
 
-    -- The size of the font in the tab bar.
-    -- Default to 10.0 on Windows but 12.0 on other systems
     font_size = 18,
 
-    -- The overall background color of the tab bar when
-    -- the window is focused
     active_titlebar_bg  = color_bg_165,
-
-    -- The overall background color of the tab bar when
-    -- the window is not focused
     inactive_titlebar_bg  = color_bg_100,
-    -- inactive_titlebar_bg  = scheme_def.brights[1],
 
     -- border_top_height = '0.03cell',
     border_bottom_height = '1px',
@@ -195,30 +132,31 @@ config.colors = {
   }
 }
 
-wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
-  local pane = tab.active_pane
-  local cwd_uri = pane.current_working_dir
-  local directoryName = 'Unknown'
-  local process = get_process(tab)
-
-  if cwd_uri then
-    -- Convert the URI to a string, remove the hostname, and decode %20s:
-    local cwd_path = cwd_uri.file_path
-
-    -- Extract the directory name from the decoded path:
-    directoryName = getDirectoryName(cwd_path)
+function tab_title(tab_info)
+  local title = tab_info.tab_title
+  -- if the tab title is explicitly set, take that
+  if title and #title > 0 then
+    return title
   end
+  -- Otherwise, use the title from the active pane
+  -- in that tab
+  return tab_info.active_pane.title
+end
 
-  local title = string.format(' %s  %s', directoryName, process) 
-
-  return {
-    { Text = title },
-  }
-end)
+wezterm.on(
+  'format-tab-title',
+  function(tab, tabs, panes, config, hover, max_width)
+    local title = tab_title(tab)
+    return {
+      { Text = string.format(' %s  ', title) },
+    }
+  end
+)
 
 -- Key Bindings
 config.keys = {
   {
+    -- Command + t for new tab
     key = "t",
     mods = "CMD",
     action = act.SpawnCommandInNewTab({ cwd = wezterm.home_dir }),
