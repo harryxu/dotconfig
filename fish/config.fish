c52118a20e80574cc326140d282e916cee73a14c
harryxu <harryzhxu@gmail.com>
2024-12-31 14:34:10 +0800

    fish: TERM=xterm-256color

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,13 @@
 set -g fish_greeting
 
 if type -q nvim
-  export EDITOR='nvim'
-  alias vim='nvim'
-  alias vi='nvim'
+    export EDITOR='nvim'
+    alias vim='nvim'
+    alias vi='nvim'
 end
 
 if test -e ~/.local/bin
-  fish_add_path -m ~/.local/bin
+    fish_add_path -m ~/.local/bin
 end
 
 alias tmux='tmux -u'
@@ -15,8 +15,8 @@ alias tmux='tmux -u'
 # pyenv
 set -Ux PYENV_ROOT $HOME/.pyenv
 if test -e $PYENV_ROOT
-  fish_add_path -m $PYENV_ROOT/bin
-  pyenv init - | source
+    fish_add_path -m $PYENV_ROOT/bin
+    pyenv init - | source
 end
 
 # bat command theme
@@ -24,6 +24,11 @@ set -Ux BAT_THEME ansi
 
 if type -q zoxide
     zoxide init fish | source
+end
+
+
+if test "$TERM_PROGRAM" = ghostty -o "$TERM_PROGRAM" = xterm-kitty
+    set -x TERM xterm-256color
 end
 
 starship init fish | source
