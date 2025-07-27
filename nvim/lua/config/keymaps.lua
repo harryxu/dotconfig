fe923d1229611248f6c06bbc74a417910314a63a
harryxu <harryzhxu@gmail.com>
2025-07-27 14:18:24 +0800

    nvim: Add nvim-unception.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -61,14 +61,14 @@ map({ "n", "i" }, "<C-.>", vim.lsp.buf.code_action, { desc = "Code Action" })
 
 -- Terminal mappings
 map({ "n" }, "<leader>jj", "<cmd>ToggleTerm direction=float<cr>", { desc = "Toggle Terminal" })
-map({ "n", "i" }, "<C-j>", "<cmd>ToggleTerm direction=horizontal<cr>", { desc = "Toggle Terminal horizontal" })
+map({ "n", "i" }, "<C-;>", "<cmd>ToggleTerm direction=horizontal<cr>", { desc = "Toggle Terminal horizontal" })
 function _G.set_terminal_keymaps()
   local opts = { buffer = 0 }
   vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-  vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts) -- Move to left window
-  vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts) -- Move to below window
-  vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts) -- Move to above windo
-  vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts) -- Move to right window
-  vim.keymap.set("t", "<C-j>", [[<cmd>ToggleTerm<cr>]], opts)
+  vim.keymap.set("t", "<C-H>", [[<Cmd>wincmd h<CR>]], opts) -- Move to left window
+  vim.keymap.set("t", "<C-J>", [[<Cmd>wincmd j<CR>]], opts) -- Move to below window
+  vim.keymap.set("t", "<C-K>", [[<Cmd>wincmd k<CR>]], opts) -- Move to above windo
+  vim.keymap.set("t", "<C-L>", [[<Cmd>wincmd l<CR>]], opts) -- Move to right window
+  vim.keymap.set("t", "<C-;>", [[<cmd>ToggleTerm<cr>]], opts)
 end
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
