0010a01cc24cb80cef736b518919e76a89975077
harryxu <harryzhxu@gmail.com>
2025-07-31 14:48:16 +0800

    nvim: config

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -7,17 +7,14 @@ return {
     "nvim-tree/nvim-web-devicons", -- optional, but recommended
   },
   lazy = false, -- neo-tree will lazily load itself
-  config = function(_, opts)
-    require("neo-tree").setup({
-      filesystem = {
-        bind_to_cwd = false,
-        follow_current_file = { enabled = false },
-        filtered_items = {
-          hide_dotfiles = false,
-          hide_gitignored = false
-        }
-      }
-    })
-  end,
-  opts = { }
+  opts = {
+    filesystem = {
+      bind_to_cwd = false,
+      follow_current_file = { enabled = false },
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+    },
+  },
 }
