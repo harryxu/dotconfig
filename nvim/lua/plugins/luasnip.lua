e182bcb784aa050fa168354de5e8910452997b62
harryxu <harryzhxu@gmail.com>
2026-02-19 16:17:13 +0800

    nvim: Update luasnip config.

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -1,19 +1,33 @@
 return {
-    "L3MON4D3/LuaSnip",
-    version = "v2.*",
-    build = "make install_jsregexp",
-    dependencies = {
-      "rafamadriz/friendly-snippets",
-      config = function()
-        require("luasnip.loaders.from_vscode").lazy_load()
+  "L3MON4D3/LuaSnip",
+  version = "v2.*",
+  build = "make install_jsregexp",
+  -- dependencies = {
+  --   "rafamadriz/friendly-snippets",
+  --   config = function()
+  --     require("luasnip.loaders.from_vscode").lazy_load()
+  --   end,
+  -- },
+  opts = {
+    history = true,
+    delete_check_events = "TextChanged",
+  },
+  keys = {
+    {
+      "<C-L>",
+      function()
+        require("luasnip").jump(1)
       end,
+      silent = true,
+      mode = { "i", "s" },
     },
-    opts = {
-      history = true,
-      delete_check_events = "TextChanged",
+    {
+      "<C-H>",
+      function()
+        require("luasnip").jump(-1)
+      end,
+      silent = true,
+      mode = { "i", "s" },
     },
-    keys = {
-      { "<C-L>", function() require("luasnip").jump(1) end,  silent = true, mode = { "i", "s" } },
-      { "<C-H>", function() require("luasnip").jump(-1) end, silent = true, mode = { "i", "s" } },
-    },
-  }
+  },
+}
