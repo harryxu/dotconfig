730cacf40c443c07424a06bf23630e6d12ddccee
harryxu <harryzhxu@gmail.com>
2026-08-31 04:36:59 +0800

    nvim: Standardize string quotes and format options in barbar.lua

--- nvim/lua/plugins-available/barbar.lua
+++ nvim/lua/plugins-available/barbar.lua
@@ -1,10 +1,13 @@
 -- The neovim tabline plugin
 return {
-  'romgrk/barbar.nvim',
+  "romgrk/barbar.nvim",
   event = "VeryLazy",
+  opts = {
+    auto_hide = 1,
+  },
   dependencies = {
-    'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
-    'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    "lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
+    "nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
   },
   -- init = function() vim.g.barbar_auto_setup = false end,
 }
