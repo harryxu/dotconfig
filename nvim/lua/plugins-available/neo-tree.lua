8e593166bcd8f4ea235166a73d2fe8a577abe403
harryxu <harryzhxu@gmail.com>
2026-08-31 12:50:37 +0800

    nvim: Update neo-tree configuration to set hijack_netrw_behavior for default sidebar

--- nvim/lua/plugins-available/neo-tree.lua
+++ nvim/lua/plugins-available/neo-tree.lua
@@ -11,6 +11,7 @@ return {
     filesystem = {
       bind_to_cwd = false,
       follow_current_file = { enabled = false },
+      hijack_netrw_behavior = "open_default", -- open as default sidebar when launching with a directory
       filtered_items = {
         hide_dotfiles = false,
         hide_gitignored = false,
